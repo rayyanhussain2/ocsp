ea7d71d7843fb163633bc7eb6ee4a9cc23e88369
Rayyan Hussain <rayyanhussain7@protonmail.com>
2023-11-12 15:12:41 +0530

    Done implementing Confidence Interval approximatio

--- shabley_shubik/simulation.c
+++ shabley_shubik/simulation.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <math.h>
 
 #define MAJORITY 272
 #define STATES 36
+#define ZVALUE 1.96f
 
 typedef struct {
     char name[40];
@@ -31,24 +33,25 @@ void swap(state* pState1, state* pState2) {
     return;
 }
 
-int returnPivotIndex(state* pStates, int size, int value){
+void incrementPivotKey(state* pStates, int size, int value){
     int sum = 0;
     for(int i = 0; i < size; i++){
         sum += (pStates + i) -> weight;
         if(sum >= value){
-            return i;
+            (pStates + i) -> key += 1;
+            return;
         }
     }
 }
 
-void heapAlgorithm(state* pStates, int size, unsigned long long* pSimulations){
+void heapAlgorithm(state* pStates, int size, unsigned long long* pSimulations, double* pMOE){
     int count[size];
     for(int i = 0; i < size; i++){
         count[i] = 0;
     }
 
     //Call for the check pivot function
-    (pStates + returnPivotIndex(pStates, size, MAJORITY)) -> key += 1;
+    incrementPivotKey(pStates, size, MAJORITY);
     *pSimulations += 1;
     
     int i = 0;
@@ -60,7 +63,7 @@ void heapAlgorithm(state* pStates, int size, unsigned long long* pSimulations){
                 swap(pStates + count[i], pStates + i);
             }
 
-            (pStates + returnPivotIndex(pStates, size, MAJORITY)) -> key += 1;
+            incrementPivotKey(pStates, size, MAJORITY);
             *pSimulations += 1;
 
             //every size simulation, check for confidence interval
@@ -71,8 +74,21 @@ void heapAlgorithm(state* pStates, int size, unsigned long long* pSimulations){
                     mean += (pStates + j) -> key; 
                 }
                 mean /= STATES;
-                printf("Mean: %f\n", mean);
+                
+                //Calculating std deviation
+                double stdDeviation = 0;
+                for(int j = 0; j < STATES; j++){
+                    stdDeviation += pow(((pStates + j) -> key) - mean, 2);
+                }
+                stdDeviation /= *pSimulations;
+                stdDeviation = sqrt(stdDeviation);
 
+                //Calculating Margin of error at 95 confidence level
+                *pMOE = ZVALUE * stdDeviation / sqrt(STATES);
+
+                if(*pMOE <= (double)8 && *pMOE >= (double)4){
+                    break;
+                }
             }
 
             count[i] += 1;
@@ -93,9 +109,8 @@ int main() {
     {"Chhattisgarh", 0, 11},
     {"Goa", 0, 2},
     {"Gujarat", 0, 26},
-    {"Haryana", 0, 20},
+    {"Haryana", 0, 10},
     {"Himachal Pradesh", 0, 4},
-    {"Jharkhand", 0, 14},
     {"Jharkhand", 0, 14},
     {"Karnataka", 0, 28},
     {"Kerala", 0, 20},
@@ -112,39 +127,43 @@ int main() {
     {"Tamil Nadu", 0, 39},
     {"Telangana", 0, 17},
     {"Tripura", 0, 2},
-    {"Uttarakhand", 0, 5},
     {"Uttar Pradesh", 0, 80},
+    {"Uttarakhand", 0, 5},
     {"West Bengal", 0, 42},
     {"Andaman and Nicobar Islands", 0, 1},
     {"Chandigarh", 0, 1},
-    {"Dadra and Nagar Haveli", 0, 1},
-    {"Daman and Diu", 0, 1},
+    {"Dadra and Nagar Haveli and Daman and Diu", 0, 2},
+    {"Jammu and Kashmir", 0, 5},
+    {"Ladakh", 0, 1},
     {"Lakshadweep", 0, 1},
     {"Delhi", 0, 7},
     {"Puducherry", 0, 1}
 };
-    FILE* pFile = fopen("output.txt", "w");
     unsigned long long simulations = 0;
+    double MOE;
 
     fputs("Starting simulation...\n", stdout);
 
     unsigned long startTime = time(NULL);
     
-    heapAlgorithm(states, STATES, &simulations);
+    heapAlgorithm(states, STATES, &simulations, &MOE);
 
     unsigned long endTime =  time(NULL);
-    unsigned long totalTime = (endTime - startTime)/1000;
+    unsigned long totalTime = (endTime - startTime);
 
     fputs("Simulation comlpeted!\n", stdout);
-    printf("%lli simulations ran in %li seconds\n", simulations, totalTime);
-    
+    printf("%lli simulations ran in %li ms\n", simulations, totalTime);
+
+    FILE* pFile = fopen("output.txt", "w");    
     fputs("Simulation Results: \n\n", pFile);
     for(int i = 0; i < STATES; i++){
         fprintf(pFile, "%s: %llu\n", states[i].name, states[i].key);
     }
     fprintf(pFile, "\nTotal simulations: %llu\n", simulations);
-    fprintf(pFile, "Runtime: %lu seconds\n", totalTime);
-    fputs("Results dumped in output.txt\n", stdout);
+    fprintf(pFile, "Runtime: %lu ms\n", totalTime);
+    fprintf(pFile, "%f percent Margin of Error at the 95 percent confidence level\n", MOE);
+    fclose(pFile); 
 
+    fputs("Results dumped in output.txt\n", stdout);
     return 0;
 }
